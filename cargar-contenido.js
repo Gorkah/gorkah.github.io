9ac05798cdfeb4396e3f339bd812fee99d321d24
Gorka Hernandez <131766898+Gorkah@users.noreply.github.com>
2023-11-07 23:10:19 +0100

    Update cargar-contenido.js

--- cargar-contenido.js
+++ cargar-contenido.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const contenidoArticulo = document.getElementById("contenido-articulo");
+    cargarContenido("article-mineria.html", "contenido-articulo-mineria");
+    cargarContenido("article-tarjetas-graficas.html", "contenido-articulo-tarjetas-graficas");
+    cargarContenido("article-overclocking.html", "contenido-articulo-overclocking");
+});
+
+function cargarContenido(url, destino) {
+    const contenidoArticulo = document.getElementById(destino);
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "article-content.html", true);
+    xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             contenidoArticulo.innerHTML = xhr.responseText;
         }
     };
     xhr.send();
-});
+}

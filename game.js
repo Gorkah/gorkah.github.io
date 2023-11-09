0e5937551543cc9cba1ed0a690600c110d21510b
Gorka Hernandez <131766898+Gorkah@users.noreply.github.com>
2023-11-09 21:01:14 +0100

    f

--- game.js
+++ game.js
@@ -1,11 +1,32 @@
 var count = 0;
 var powerups = 0;
 var powerups2 = 0;
+var powerups3 = 0;
 function clickCookie() {
-    count += 1 + 10 * powerups2;
+    count += 1 + 10 * powerups2 + 50* powerups3;
     document.getElementById('counter').innerHTML = count;
+    createCrumbs();
     updateStats();
 }
+// Función para crear migas
+function createCrumbs() {
+    var crumbs = document.createElement('div');
+    crumbs.className = 'crumbs';
+    document.getElementById('game').appendChild(crumbs);
+
+    // Posición aleatoria para las migas
+    var xPos = Math.random() * 50 + 25;
+    var yPos = Math.random() * 50 + 25;
+
+    crumbs.style.left = xPos + 'vw';
+    crumbs.style.top = yPos + 'vh';
+
+    // Elimina la migaja después de un tiempo
+    setTimeout(function() {
+        crumbs.remove();
+    }, 1000);
+}
+
 function buyPowerup() {
     if (count >= 25) {
         count -= 25;
@@ -24,6 +45,14 @@ function buyPowerup2() {
         updateStats();
     }
 }
-function updateStats() {
-    document.getElementById('stats').innerHTML = 'Power-Ups: ' + powerups + ', Power-Ups 2: ' + powerups2 + ', Clics por hora: ' + ((1 + 10 * powerups2) * 60 * 60 + powerups * 2 * 60 * 60);
+function buyPowerup3(){
+    if (count >= 300) {
+        count -= 300;
+        powerups3 += 1;
+        updateStats();
+    }
 }
+function updateStats() {
+    document.getElementById('stats').innerHTML = 'Power-Ups: ' + powerups + ', Power-Ups 2: ' + powerups2 + ', Power-Ups 3: ' + powerups3 +', Clics por hora: ' + ((0 + 10 * powerups2) * 60 * 60 + powerups * 2 * 60 * 60 + (powerups3*10*60*60));
+}
+
